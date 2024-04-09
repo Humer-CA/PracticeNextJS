0787fc0a6350b65393d2694d75457b78f62c1452
Humer-CA <humeraltares@gmail.com>
2024-04-09 18:31:25 +0800

    added get, create and update routes

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -9,7 +9,20 @@ export function GET(request: NextRequest, { params }: { params: { id: number } }
   return NextResponse.json({ id: 1, name: "ASD" });
 }
 
-export async function POST(request: NextRequest) {
+export async function PUT(request: NextRequest, { params }: { params: { id: number } }) {
+  // Validate the request body
   const body = await request.json();
-  return NextResponse.json(body);
+
+  // If invalid, return 400
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  }
+
+  // Fetch the user with the given id
+  // If doesn't exist, return 404
+  if (params.id > 10) return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  // Update the user
+  // Return the updated user
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
