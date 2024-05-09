c35d90b6a20bf94d1c53be64dd39b9822fc04251
Humer-CA <humeraltares@gmail.com>
2024-05-09 16:49:29 +0800

    refactored get and post method for users routes

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,18 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
+import prisma from "../../../prisma/client";
 
 // fetch users from db
-// ? if no caching remove (request: NextRequest)
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Doe" },
-    { id: 2, name: "John" },
-  ]);
+// ? if no caching remove (request: NextRequest
+export async function GET(request: NextRequest) {
+  const users = await prisma.user.findMany();
+  return NextResponse.json(users);
 }
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = schema.safeParse(body);
   if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 });
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+
+  const user = await prisma.user.findUnique({ where: body.email });
+  if (user) NextResponse.json({ error: "Email already exists" }, { status: 400 });
+
+  const createUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+  return NextResponse.json(createUser, { status: 201 });
 }
